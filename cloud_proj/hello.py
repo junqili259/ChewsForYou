089ecc057beffa1176b71336bb15188162bf8020
junqili <junqili259@gmail.com>
2020-03-17 18:05:12 -0400

    update
    added some api endpoint for test

--- cloud_proj/hello.py
+++ cloud_proj/hello.py
@@ -1,25 +1,31 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 from forms import AddressForm
 import requests
+from api import business_search
 
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '53ba0078b3c38695e0697cf2f4c8bd79'
 
+# Testing api endpoints
 @app.route("/apitest")
 def hello_world():
-    #return render_template('home.html')
-    r = requests.get('https://api.openweathermap.org/data/2.5/weather?q=London&appid=93a0a96f6526590b3123ee6d5ea16fa9')
-    r_dict = r.json()
-    return r_dict['main']
-
+    data = business_search()
+    return data
+    
 
 #Home Page with an address form
 @app.route('/', methods=['GET','POST'])
 def submitAddress():
     addressForm = AddressForm()
     if addressForm.validate_on_submit():
+        address = request.form.get('address')
+        #city = request.form.get('city')
+        #state = request.form.get('state')
+        #zipcode = request.form.get('zipcode')
+        #print(address, city, state, zipcode)
+        #print(city)
         return redirect(url_for('newpage'))
     return render_template('home.html', form=addressForm)
     
