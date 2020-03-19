6e2db46a29b304ea0ead9f62300fb4a2405121cf
junqili <junqili259@gmail.com>
2020-03-18 20:28:10 -0400

    Update
    Restaurant randomrizer implemented

--- cloud_proj/routes.py
+++ cloud_proj/routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, redirect, request
 from forms import AddressForm
-import requests
+import requests, random
 from api import business_search
 
 
@@ -8,25 +8,28 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '53ba0078b3c38695e0697cf2f4c8bd79'
 
-# Testing api endpoints
 @app.route("/apitest")
 def hello_world():
-    data = business_search()
-    return data
-    
+    address = ''
+    data = business_search(address) 
+    randomRestaurant = random.choice(data)
+    return redirect(randomRestaurant)
+
 
 #Home Page with an address form
 @app.route('/', methods=['GET','POST'])
 def submitAddress():
     addressForm = AddressForm()
     if addressForm.validate_on_submit():
+        fullAddress = ''
         address = request.form.get('address')
-        #city = request.form.get('city')
-        #state = request.form.get('state')
-        #zipcode = request.form.get('zipcode')
-        #print(address, city, state, zipcode)
-        #print(city)
-        return redirect(url_for('newpage'))
+        city = request.form.get('city')
+        state = request.form.get('state')
+        zipcode = request.form.get('zipcode')
+        fullAddress = address + ", " + city + "," + state + " " + zipcode
+        data = business_search(fullAddress) 
+        randomRestaurant = random.choice(data)
+        return redirect(randomRestaurant)
     return render_template('home.html', form=addressForm)
     
 
