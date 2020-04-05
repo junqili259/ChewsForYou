348f8d3ebeb70c6ec86fae14f29cd60a28566ddb
junqili <39481617+junqili259@users.noreply.github.com>
2020-04-05 13:21:19 -0400

    added new routes

--- cloud_proj/forms.py
+++ cloud_proj/forms.py
@@ -2,9 +2,21 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextField, SubmitField
 from wtforms.validators import DataRequired, Length
 
+#  Form for the user to input address details
 class AddressForm(FlaskForm):
     address = StringField('Address', validators=[DataRequired()])
     city = StringField('City', validators=[DataRequired()])
     state = StringField('State', validators=[DataRequired()])
     zipcode = StringField('Zip Code', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+# Form for the user to input register details
+class RegisterForm(FlaskForm):
+    firstName = StringField('First Name',validators=[DataRequired()])
+    lastName = StringField('Last Name',validators=[DataRequired()])
+    email = StringField('Email',validators=[DataRequired()])
+    confirmEmail = StringField('Confirm Email', validators=[DataRequired()])
+    username = StringField('Username',validators=[DataRequired()])
+    password = StringField('Password',validators=[DataRequired()])
+    confirmPassword = StringField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
--- cloud_proj/routes.py
+++ cloud_proj/routes.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, redirect, request
-from forms import AddressForm
+from forms import AddressForm, RegisterForm
 import requests, random
 from api import business_search
 
@@ -8,6 +8,8 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '53ba0078b3c38695e0697cf2f4c8bd79'
 
+# For testing api functions only
+# Will not be in final implementation
 @app.route("/apitest")
 def hello_world():
     address = ''
@@ -16,23 +18,47 @@ def hello_world():
     return redirect(randomRestaurant)
 
 
-#Home Page with an address form
+# Address Form page where user enters address information to obtain a random eatery in response
 @app.route('/', methods=['GET','POST'])
 def submitAddress():
+    
+    # Create an address form object
     addressForm = AddressForm()
+    
+    # If user clicks submit button
     if addressForm.validate_on_submit():
         fullAddress = ''
+        
+        # Store user inputs to their corresponding variables
         address = request.form.get('address')
         city = request.form.get('city')
         state = request.form.get('state')
         zipcode = request.form.get('zipcode')
+        
+        # Create full address string
         fullAddress = address + ", " + city + "," + state + " " + zipcode
+        
+        # Pass the full address to an api function call and receive data back
         data = business_search(fullAddress) 
+        
+        # Return a random restaurant url from the data
         randomRestaurant = random.choice(data)
+        
+        # redirect user to that url
         return redirect(randomRestaurant)
     return render_template('home.html', form=addressForm)
     
 
-@app.route("/newpage")
-def newpage():
-    return render_template('newpage.html')
+# Login page for users with a pre-existing account
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+
+# Signing up for a new account
+@app.route("/register", methods=['GET','POST'])
+def register():
+
+    # Create a register form object
+    registerForm = RegisterForm()
+    return render_template('register.html', form=registerForm)
